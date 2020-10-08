3d0295f7cba83246924e175b9c50308aad9c04ba
Shahid N. Shah <shahid@shah.org>
2020-10-07 22:32:12 -0400

    Delete unused file

--- core/runtime.ts
+++
@@ -1,6 +0,0 @@
-export type FileStats = Deno.FileInfo;
-export type FileType = string;
-export type Writer = Deno.Writer;
-
-export const mkdirSync = Deno.mkdirSync;
-export const makeTempDirSync = Deno.makeTempDirSync;
