6100c25adae2c263dfb4e3235505e8eefefc9bb6
Shahid N. Shah <shahid@shah.org>
2020-10-19 10:03:37 -0400

    fix: remove Deno std@v from dependencies

--- core/deps.ts
+++ core/deps.ts
@@ -1,8 +1,8 @@
-export * as fs from "https://deno.land/std@v0.74.0/fs/mod.ts";
-export * as io from "https://deno.land/std@v0.74.0/io/mod.ts";
-export * as path from "https://deno.land/std@v0.74.0/path/mod.ts";
-export * as uuid from "https://deno.land/std@v0.74.0/uuid/mod.ts";
+export * as fs from "https://deno.land/std@0.74.0/fs/mod.ts";
+export * as io from "https://deno.land/std@0.74.0/io/mod.ts";
+export * as path from "https://deno.land/std@0.74.0/path/mod.ts";
+export * as uuid from "https://deno.land/std@0.74.0/uuid/mod.ts";
 export * as mediaTypes from "https://deno.land/x/media_types@v2.5.1/mod.ts";
 export * as randomUserAgent from "https://denopkg.com/often/rua@v1.0/mod.js";
-export * as safety from "https://denopkg.com/shah/ts-safety@v0.2.4/mod.ts";
+export * as safety from "https://denopkg.com/shah/ts-safety@v0.2.5/mod.ts";
 export * as queryableHTML from "../queryable-html/mod.ts";
