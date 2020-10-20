c8557969ce21ddc4f2f9663a3c35c30a96d7dbc4
Shahid N. Shah <shahid@shah.org>
2020-10-20 16:34:32 -0400

    fix: update dependencies

--- git/deps.ts
+++ git/deps.ts
@@ -1,7 +1,5 @@
 export * as urlcat from "https://denopkg.com/balazsbotond/urlcat@v2.0.4/src/index.ts";
-export * as safety from "https://denopkg.com/shah/ts-safety@v0.2.5/mod.ts";
+export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.0/mod.ts";
 export * as safeHttpClient from "../core/mod.ts";
-// export * as inspect from "../../ts-inspect/mod.ts";
-// export * as inspectText from "../../ts-inspect/text/mod.ts";
-export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.8.2/mod.ts";
-export * as inspText from "https://denopkg.com/shah/ts-inspect@v0.8.2/text/mod.ts";
+export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.8.4/mod.ts";
+export * as inspText from "https://denopkg.com/shah/ts-inspect@v0.8.4/text/mod.ts";
