8bf15a3464ee9ddacf5edb861bdecf381849bfd4
Shahid N. Shah <shahid@shah.org>
2020-10-25 14:48:56 -0400

    fix: bump dependencies

--- queryable-html/deps.ts
+++ queryable-html/deps.ts
@@ -1,2 +1,2 @@
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.0/mod.ts";
-export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.8.2/mod.ts";
+export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.8.6/mod.ts";
