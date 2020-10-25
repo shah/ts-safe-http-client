fa9575298c629a9ee3cdfd690d54177f7c658640
Shahid N. Shah <shahid@shah.org>
2020-10-25 16:23:24 -0400

    fix: bump dependencies

--- git/deps.ts
+++ git/deps.ts
@@ -1,5 +1,5 @@
 export * as urlcat from "https://denopkg.com/balazsbotond/urlcat@v2.0.4/src/index.ts";
-export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.8.6/mod.ts";
-export * as inspText from "https://denopkg.com/shah/ts-inspect@v0.8.6/text/mod.ts";
+export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.8.7/mod.ts";
+export * as inspText from "https://denopkg.com/shah/ts-inspect@v0.8.7/text/mod.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.0/mod.ts";
 export * as safeHttpClient from "../core/mod.ts";
