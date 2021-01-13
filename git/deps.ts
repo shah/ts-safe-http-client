86f3c3109734542aba1311358b2fa2c50b817e90
geo_vl <geo@citrusinformatics.com>
2021-01-13 18:45:53 +0530

    update: deps packages to latest version

--- git/deps.ts
+++ git/deps.ts
@@ -1,6 +1,6 @@
 export * as urlcat from "https://denopkg.com/balazsbotond/urlcat@v2.0.4/src/index.ts";
-export * as vault from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.9/vault.ts";
-export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.8.9/mod.ts";
-export * as inspText from "https://denopkg.com/shah/ts-inspect@v0.8.9/text/mod.ts";
+export * as vault from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.10/vault.ts";
+export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.9.1/mod.ts";
+export * as inspText from "https://denopkg.com/shah/ts-inspect@v0.9.1/text/mod.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.1/mod.ts";
 export * as safeHttpClient from "../core/mod.ts";
--- queryable-html/deps.ts
+++ queryable-html/deps.ts
@@ -1,2 +1,2 @@
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.1/mod.ts";
-export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.8.9/mod.ts";
+export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.9.1/mod.ts";
