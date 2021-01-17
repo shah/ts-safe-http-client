893bbac999fee956f454de18964744f0c4bf5410
Shahid N. Shah <shahid@shah.org>
2021-01-16 21:18:00 -0500

    fix: update dependency

--- git/deps.ts
+++ git/deps.ts
@@ -1,5 +1,5 @@
 export * as urlcat from "https://denopkg.com/balazsbotond/urlcat@v2.0.4/src/index.ts";
-export * as vault from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.10/vault.ts";
+export * as vault from "https://denopkg.com/gov-suite/governed-service-helpers@v0.3.11/vault.ts";
 export * as inspect from "https://denopkg.com/shah/ts-inspect@v0.9.1/mod.ts";
 export * as inspText from "https://denopkg.com/shah/ts-inspect@v0.9.1/text/mod.ts";
 export * as safety from "https://denopkg.com/shah/ts-safety@v0.3.1/mod.ts";
